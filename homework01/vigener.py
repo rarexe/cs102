9cc5b6994c3d8c2e85c8996f73241d42394284d1
AnnaRedichkina <55876456+AnnaRedichkina@users.noreply.github.com>
2019-10-06 21:52:39 +0300

    Update vigener.py

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -13,15 +13,15 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     for i, a in enumerate(plaintext):
         if ord('a') <= ord(a) <= ord('z'):
             x = ord(a) + ord(key[i % len(key)]) - ord('a')
-        if x > ord('z'):
-            x -= 26
-            ciphertext = chr(x)
+            if x > ord('z'):
+                x -= 26
+                ciphertext = chr(x)
         else :
-             ord('A') <= ord(a) <= ord('Z')
-             x = ord(a) + ord(key[i % len(key)]) - ord('a')
-        if x > ord('z'):
-             x -= 26
-             ciphertext = chr(x)
+            ord('A') <= ord(a) <= ord('Z')
+            x = ord(a) + ord(key[i % len(key)]) - ord('a')
+            if x > ord('z'):
+                x -= 26
+                ciphertext = chr(x)
     return ciphertext
 
 
@@ -41,13 +41,13 @@ def decrypt_vigenere(ciphertext, keyword):
     for i, a in enumerate(ciphertext):
         if ord('a') <= ord(a) <= ord('z'):
             x = ord(a) - ord(key[i % len(key)]) + ord('a')
-        if x < ord('a'):
-            x += 26
-            plaintext = chr(x)
+            if x < ord('a'):
+                x += 26
+                plaintext = chr(x)
         else:
             ord('A') <= ord(a) <= ord('Z')
             x = ord(a) - ord(key[i % len(key)]) + ord('a')
-        if x < ord('A'):
-            x += 26
-            plaintext = chr(x)
+            if x < ord('A'):
+                x += 26
+                plaintext = chr(x)
     return plaintext
