d53955f4e285ecf2a7ed397109ca58dab6e73cf4
AnnaRedichkina <55876456+AnnaRedichkina@users.noreply.github.com>
2019-10-06 18:11:14 +0300

    Add files via upload

---
+++ homework01/caesar.py
@@ -0,0 +1,45 @@
+def encrypt_caesar(plaintext: str) -> str:
+"""
+»> encrypt_caesar("PYTHON")
+'SBWKRQ'
+»> encrypt_caesar("python")
+'sbwkrq'
+»> encrypt_caesar("Python3.6")
+'Sbwkrq3.6'
+»> encrypt_caesar("")
+''
+"""
+
+
+plaintext = input("введите текст для зашифровки: ")
+for c in plaintext:
+if ord('a') <= ord(c) <= ord('w') or ord('A') <= ord(c) <= ord('W'):
+ciphhertext = chr(ord(c) + 3)
+print(ciphhertext, end='')
+if ord('x') <= ord(c) <= ord('z') or ord('X') <= ord(c) <= ord('Z'):
+ciphertext = chr(ord(c) + 3 - 26)
+print(ciphertext, end='')
+
+
+
+def decrypt_caesar(ciphertext: str) -> str:
+"""
+»> decrypt_caesar("SBWKRQ")
+'PYTHON'
+»> decrypt_caesar("sbwkrq")
+'python'
+»> decrypt_caesar("Sbwkrq3.6")
+'Python3.6'
+»> decrypt_caesar("")
+''
+"""
+
+
+ciphhertext = input('\n' 'введите текст для расшифровки: ')
+for c in ciphhertext:
+if ord('d') <= ord(c) <= ord('z') or ord('D') <= ord(c) <= ord('Z') :
+plaintext = chr(ord(c) - 3)
+print(plaintext, end='')
+if ord('a') <= ord(c) <= ord('c') or ord('A') <= ord(c) <= ord('C'):
+plaintext = chr(ord(c) - 3 + 26)
+print(plaintext, end='')
---
+++ homework01/vigener.py
@@ -0,0 +1,55 @@
+
+def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+    """
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    """
+plaintext = input('Введите текст для шифрования: ')
+key = input('Введите ключ: ')
+for i, a in enumerate(plaintext):
+    if ord('a') <= ord(a) <= ord('z'):
+        x = ord(a) + ord(key[i % len(key)]) - ord('a')
+        if x > ord('z'):
+            x -= 26
+        ciphertext = chr(x)
+        print(ciphertext, end='')
+    else:
+        ord('A') <= ord(a) <= ord('Z')
+        x = ord(a) + ord(key[i % len(key)]) - ord('a')
+        if x > ord('z'):
+             x -= 26
+        ciphertext = chr(x)
+        print(ciphertext, end='')
+
+
+
+def decrypt_vigenere(ciphertext, keyword):
+    """
+    Decrypts a ciphertext using a Vigenere cipher.
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    """
+ciphertext = input('\n' 'Введите текст для расшифровки: ')
+key = input('Введите ключ: ')
+for i, a in enumerate(ciphertext):
+    if ord('a') <= ord(a) <= ord('z'):
+        x = ord(a) - ord(key[i % len(key)]) + ord('a')
+        if x < ord('a'):
+            x += 26
+        plaintext = chr(x)
+        print(plaintext, end='')
+    else:
+        ord('A') <= ord(a) <= ord('Z')
+        x = ord(a) - ord(key[i % len(key)]) + ord('a')
+        if x < ord('A'):
+             x += 26
+        plaintext = chr(x)
+        print(plaintext, end='')
