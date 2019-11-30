766018f072d5437cefd197839f4c969b09d4cba5
JBL123-cell <55876456+JBL123-cell@users.noreply.github.com>
2019-11-30 14:47:10 +0300

    Add files via upload

---
+++ homework05/config.py
@@ -0,0 +1,10 @@
+VK_CONFIG = {
+    'domain': 'https://api.vk.com/method',
+    'access_token': '291757c1291757c1291757c1b92979648e22917291757c174cca73801b0ba2473c4ba84',
+    'version': '5.103',
+}
+
+PLOTLY_CONFIG = {
+    'username': 'Имя пользователя Plot.ly',
+    'api_key': 'Ключ доступа Plot.ly',
+}
